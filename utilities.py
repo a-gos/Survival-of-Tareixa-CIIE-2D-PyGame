b0e04d09fbb8cc205f0463d2bc42e026a70b5a15
Santiago Fernández González <santiago.fernandezg@udc.es>
2020-03-16 20:23:58 +0100

    Correxidos os erros do merge

--- utilities.py
+++ utilities.py
@@ -7,35 +7,29 @@ from resourcesmanager import ResourcesManager
 class Bullet(MySprite):
     def __init__(self,player,speed, scrollx):
         MySprite.__init__(self)
-      #  self.rect = rect
-        self.rect = pygame.Rect(4,4, 50, 12)
+        self.image = ResourcesManager.LoadImage('characters','disparo.png', -1)
+
+        self.rect = self.image.get_rect()
         (posx, posy) = player.position
         self.setposition((posx, player.rect.centery))
         self.setpositionscreen(( scrollx, 0 ))
 
-        # Establecer la orientación, la posición y la velocidad del sprite
         self.looking = player.looking
-        pos_y = player.rect.centery
         if(self.looking == LEFT):
-            pos_x = player.rect.left - self.rect.width
-            self.setposition((pos_x, pos_y))
-            self.image = pygame.transform.flip(self.image, 1, 0)
+            self.image = pygame.transform.flip(self.image, 1 ,0)
             speed = -speed
-        else:
-            pos_x = player.rect.right
-            self.setposition((pos_x, pos_y))
 
         self.speed = (speed,0)
-        print("spawn: ", self.position)
-
+        # print("spawn: ", self.position)
+    #
 
     def update(self, platformGroup, enemyGroup, scroll, time):
         # Comprueba si sobrepasa los límites de la pantalla
         if self.looking == LEFT and self.rect.right < 0:
-            print("position left: ", self.position, "rect: ", self.rect.right)
+            # print("position left: ", self.position, "rect: ", self.rect.right)
             self.kill()
         elif self.looking == RIGHT and self.rect.left > WIDTH_SCREEN:
-            print("position right: ", self.position, "rect: ", self.rect.left)
+            # print("position right: ", self.position, "rect: ", self.rect.left)
             self.kill()
         else:
             # Comprueba si colisiona con una plataforma o enemigo
