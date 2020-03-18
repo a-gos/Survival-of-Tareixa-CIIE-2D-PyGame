35d6df1a364f1e3a3447850f09c8a742bfdca84a
Santiago Fernández González <santiago.fernandezg@udc.es>
2020-03-18 10:40:09 +0100

    Cambio na imaxe dos bloques das plataformas

--- data/levels/tiles.tsx
+++ data/levels/tiles.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.3" name="Tiles" tilewidth="32" tileheight="32" spacing="1" tilecount="9" columns="3">
- <image source="../scene/tiles_platforms.png" width="98" height="98"/>
+<tileset version="1.2" tiledversion="1.3.3" name="Tiles" tilewidth="32" tileheight="32" spacing="1" tilecount="16" columns="4">
+ <image source="../scene/tiles_platforms.png" width="131" height="131"/>
 </tileset>
