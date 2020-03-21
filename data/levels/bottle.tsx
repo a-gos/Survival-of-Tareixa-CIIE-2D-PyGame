156d8986c5d3eecd5f845fdd06b53440bf2c95df
Santiago Fernández González <santiago.fernandezg@udc.es>
2020-03-21 17:05:41 +0100

    Engadir archivos de bloques para os obxectos dos mapas

---
+++ data/levels/bottle.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="Bottle" tilewidth="24" tileheight="35" tilecount="1" columns="1">
+ <image source="../scene/bottle.png" width="24" height="35"/>
+</tileset>
---
+++ data/levels/handgun.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="Handgun" tilewidth="25" tileheight="25" tilecount="1" columns="1">
+ <image source="../scene/handgun.png" width="25" height="25"/>
+</tileset>
